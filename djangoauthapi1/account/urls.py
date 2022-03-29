8fecacdc4a8aa7af3e02175ec7c490bf6c2ab317
Mamun <a.a.mamunbu@gmail.com>
2022-03-29 11:03:44 +0600

    custom user bug fixed

--- djangoauthapi1/account/urls.py
+++ djangoauthapi1/account/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
+from .views import UserRegistrationView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('register/', UserRegistrationView.as_view(),name='register'),
 
 ]
