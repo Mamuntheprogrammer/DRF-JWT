068634784fdbc1ef9a37dd04c94f948f3ac2eb0e
Mamun <a.a.mamunbu@gmail.com>
2022-04-06 10:47:42 +0600

    Change Password Complete

--- djangoauthapi1/account/views.py
+++ djangoauthapi1/account/views.py
@@ -1,20 +1,40 @@
 from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from .serializers import UserRegistrationSerializer,UserloginSerializer
+from .serializers import UserRegistrationSerializer, UserloginSerializer, UserProfileSerializer, \
+    UserChangePasswordSerializer
+from .renders import UserRender
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+
+
+
 
 # Create your views here.
 
 class UserRegistrationView(APIView):
+    renderer_classes = [UserRender]
     def post(self,request,format=None):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             user=serializer.save()
-            return Response({'msg':'Registration ok'},status=status.HTTP_201_CREATED)
+            token=get_tokens_for_user(user)
+            return Response({'token':token,'msg':'Registration ok'},status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 class UserLoginView(APIView):
+    renderer_classes = [UserRender]
     def post(self,request,format=None):
         serializer = UserloginSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
@@ -22,8 +42,25 @@ class UserLoginView(APIView):
             password = serializer.data.get('password')
         user = authenticate(email=email,password=password)
         if user is not None:
-            return Response({'msg':'logedIn seccessful'},status=status.HTTP_200_OK)
+            token = get_tokens_for_user(user)
+            return Response({'token':token,'msg':'logedIn seccessful'},status=status.HTTP_200_OK)
         else:
             return Response({'msg': 'Please check yout entered info '}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class UserProfileView(APIView):
+    renderer_classes = [UserRender]
+    permission_classes = [IsAuthenticated]
+    def get(self,request,format=None):
+        serializer = UserProfileSerializer(request.user)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+
+class UserChangePasswordView(APIView):
+    renderer_classes = [UserRender]
+    permission_classes = [IsAuthenticated]
+    def post(self,request,format=None):
+        serializer = UserChangePasswordSerializer(data=request.data,context={'user':request.user})
+        if serializer.is_valid(raise_exception=True):
+            return Response({'msg':'Password Changed Successfully'},status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
